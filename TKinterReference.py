a88c6c385758c4b1162409eb37ea5657502fcb5d
Nicholas Eh <SentinelGolem@Gmail.com>
2020-06-30 20:40:06 -0400

    Created Class ReferenceApp
    Transitioned to object-oriented program
    Created two labels and two buttons, the buttons change the text of the 
    second label.

--- TKinterReference.py
+++ TKinterReference.py
@@ -1,6 +1,30 @@
 from tkinter import *
 from tkinter import ttk
 
+class ReferenceApp:
 
-root = Tk()
-root.mainloop()
+    def __init__(self, master):
+
+        self.label = ttk.Label(master, text = "This will be a reference page!")
+        self.label.grid(row = 0, column = 0, columnspan = 2)
+
+        self.labelRefer = ttk.Label(master, text = "")
+        self.labelRefer.grid(row = 1, column = 0, columnspan = 2)
+
+        ttk.Button(master, text = "Button", command = self.refer_button).grid(row = 2, column = 0)
+
+        ttk.Button(master, text = "Label", command = self.refer_label).grid(row = 2, column = 1)
+
+    def refer_button(self):
+        self.labelRefer.config(text = "A button is a clicky thing that does stuff when clicked!")
+
+    def refer_label(self):
+        self.labelRefer.config(text = "A label is something that just shows text.")
+
+def main():
+
+    root = Tk()
+    app = ReferenceApp(root)
+    root.mainloop()
+
+if __name__ == "__main__": main()
