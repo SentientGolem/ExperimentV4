e795fda020e491be9fc9d34197683d103d5733e2
Nicholas Eh <SentinelGolem@Gmail.com>
2020-06-30 22:14:40 -0400

    Adds Image Adds image "Chrome.png" along with a button to display it.

--- TKinterReference.py
+++ TKinterReference.py
@@ -8,18 +8,26 @@ class ReferenceApp:
         self.label = ttk.Label(master, text = "This will be a reference page!")
         self.label.grid(row = 0, column = 0, columnspan = 2)
 
-        self.labelRefer = ttk.Label(master, text = "")
+        self.labelRefer = ttk.Label(master, text = "", wraplength = 200, justify = CENTER)
+        self.labelRefer.config(foreground = 'cyan', background = 'purple')
         self.labelRefer.grid(row = 1, column = 0, columnspan = 2)
+
+        self.image = PhotoImage(file = ".\\chrome.png")
 
         ttk.Button(master, text = "Button", command = self.refer_button).grid(row = 2, column = 0)
 
         ttk.Button(master, text = "Label", command = self.refer_label).grid(row = 2, column = 1)
 
+        ttk.Button(master, text = "Picture", command = self.refer_picture).grid(row = 3, column = 0)
+
     def refer_button(self):
-        self.labelRefer.config(text = "A button is a clicky thing that does stuff when clicked!")
+        self.labelRefer.config(text = "A button is a clicky thing that does stuff when clicked!", image = "")
 
     def refer_label(self):
-        self.labelRefer.config(text = "A label is something that just shows text.")
+        self.labelRefer.config(text = "A label is something that just shows text.", image = "")
+
+    def refer_picture(self):
+        self.labelRefer.config(image = self.image, text = "LOOKIT THIS BUTTON", compound = "center")
 
 def main():
 
